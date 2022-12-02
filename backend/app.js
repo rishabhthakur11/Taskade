250201694e386853d8666c36eb3271caaf17c811
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2022-12-02 15:24:14 +0530

    routes updated

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./config/database");
 const app = express();
@@ -11,6 +11,5 @@ connectToDB(); // added
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/todos", require("./routes/api/todos"));
-app.use("/api/tags", require("./routes/api/tags"));
 
 module.exports = app;
